8daef85d144989e65c69446ded40de1152ca05bf
Krtoeezi <1175107342@qq.com>
2023-03-20 20:05:08 +0800

    G to g

--- src/midend/mir_lower.cpp
+++ src/midend/mir_lower.cpp
@@ -4,7 +4,7 @@
 
 #include <graphitron/midend/mir_lower.h>
 #include <graphitron/midend/edgeset_lower.h>
-#include <Graphitron/midend/prop_array_lower.h>
+#include <graphitron/midend/prop_array_lower.h>
 
 namespace graphitron {
     void MIRLower::lower(MIRContext* mir_context, ScheduleSpace* schedule) {
