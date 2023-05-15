e199a11f8dc7d185791d3df6be4f9420720619f8
Krtoeezi <1175107342@qq.com>
2023-05-15 16:14:51 +0800

    mv to 2 lib

---
+++ lib/1/libgraph/xcl/xcl.mk
@@ -0,0 +1,4 @@
+xcl_SRCS:=${FPGA_PATH}/libgraph/xcl/xcl.c
+xcl_HDRS:=${FPGA_PATH}/libgraph/xcl/xcl.h
+
+xcl_CXXFLAGS:=-I${FPGA_PATH}/libgraph/xcl
--- mks/utils/compile.mk
+++ mks/utils/compile.mk
@@ -2,11 +2,9 @@ compile: cleanbuild
 	-$(MK) $(FPGA_APP_PATH)
 	-$(RMDIR) $(FPGA_APP_PATH)/../tmp/
 	-$(MK) $(FPGA_APP_PATH)/../tmp/
-ifeq ($(strip $(OPT)), 1)
-	$(CP) $(ABS_ROOT_REPO)/lib/template/app/* $(FPGA_APP_PATH)
-	$(CP) $(ABS_ROOT_REPO)/lib/template/kernel $(FPGA_PATH)
-endif
-	$(CP) $(ABS_ROOT_REPO)/lib/libfpga $(FPGA_PATH)
-	$(CP) $(ABS_ROOT_REPO)/lib/libgraph $(FPGA_PATH)
+	$(CP) $(ABS_ROOT_REPO)/lib/$(OPT)/template/app/* $(FPGA_APP_PATH)
+	$(CP) $(ABS_ROOT_REPO)/lib/$(OPT)/template/kernel $(FPGA_PATH)
+	$(CP) $(ABS_ROOT_REPO)/lib/$(OPT)/libfpga $(FPGA_PATH)
+	$(CP) $(ABS_ROOT_REPO)/lib/$(OPT)/libgraph $(FPGA_PATH)
 	python3 ./graphitron_compiler/graphitronc.py -f ${APP_INPUT} -o ${FPGA_APP_PATH} -p $(OPT)
 	-$(RMDIR) $(FPGA_APP_PATH)/../tmp/
